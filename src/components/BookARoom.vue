c4d258eeb1ce79a02e84172265d59293bd70f043
Lin Pei Wen <mobilesuit0922@gmail.com>
2020-11-03 18:19:11 +0800

    API 串接成功，將部分樣式改為 BEM 設計模式，偽元素的 content 還沒解決，不過找到方法了。

--- src/components/BookARoom.vue
+++ src/components/BookARoom.vue
@@ -14,59 +14,35 @@
             </article>
             <section class="homepageConfiguration_main_context col-9">
                 <div class="homepageConfiguration_main_context_thumbnail">
-                    <div class="thumbnail col-4" :style="{ backgroundImage: `url(${ Room1 })` }"></div>
-                    <div class="thumbnail col-4" :style="{ backgroundImage: `url(${ Room2 })` }"></div>
-                    <div class="thumbnail col-4" :style="{ backgroundImage: `url(${ Room3 })` }"></div>
-                    <div class="thumbnail col-4" :style="{ backgroundImage: `url(${ Room4 })` }"></div>
-                    <div class="thumbnail col-4" :style="{ backgroundImage: `url(${ Room5 })` }"></div>
-                    <div class="thumbnail col-4" :style="{ backgroundImage: `url(${ Room6 })` }"></div>
+                    <div class="thumbnail col-4" v-for="(item, key) in totalRooms" :key="key" :style="{ backgroundImage: `url(${ item.imageUrl })` }">{{ item.name }}</div>
                 </div>
             </section>
         </div>
-        <div class="homepageConfiguration_other">
-            <button><i class="fas fa-circle"></i></button>
-            <button><i class="far fa-circle"></i></button>
-            <button><i class="far fa-circle"></i></button>
-            <button><i class="far fa-circle"></i></button>
+        <div class="homepageConfiguration_other homepageConfiguration_other--color">
+            <button class="homepageConfiguration_other_btn homepageConfiguration_other_btn--style"><i class="fas fa-circle"></i></button>
+            <button class="homepageConfiguration_other_btn homepageConfiguration_other_btn--style"><i class="far fa-circle"></i></button>
+            <button class="homepageConfiguration_other_btn homepageConfiguration_other_btn--style"><i class="far fa-circle"></i></button>
+            <button class="homepageConfiguration_other_btn homepageConfiguration_other_btn--style"><i class="far fa-circle"></i></button>
         </div>
     </div>
 </template>
 <script>
 import BG1 from "../assets/image/BG1.png";
-import Room1 from '../assets/image/room1.png';
-import Room2 from '../assets/image/room2.png';
-import Room3 from '../assets/image/room3.png';
-import Room4 from '../assets/image/room4.png';
-import Room5 from '../assets/image/room5.png';
-import Room6 from '../assets/image/room6.png';
 
 export default {
     data() {
         return {
             API: 'https://challenge.thef2e.com/api/thef2e2019/stage6/rooms',
             token: process.env.VUE_APP_TOKEN,
-            BG1,
-            Room1,
-            Room2,
-            Room3,
-            Room4,
-            Room5,
-            Room6
+            totalRooms: [],
+            BG1
         };
     },
-    // methods: {
-    //     // getAllRooms() {
-    //     //     const vm = this;
-    //     //     // const api = 'https://challenge.thef2e.com/api/thef2e2019/stage6/rooms';
-    //     //     vm.$http.get(vm.API).then(response => {
-    //     //         console.log(response.data)
-    //     //     })
-    //     // }
-    // },
-    // created() {
-    //     this.$http.get(this.API).then(response => {
-    //         console.log(response.data);
-    //     })
-    // }
+    created() {
+        this.$http.get(this.API).then(response => {
+            this.totalRooms = response.data.items;
+            console.log(this.totalRooms)
+        })
+    }
 };
 </script>
