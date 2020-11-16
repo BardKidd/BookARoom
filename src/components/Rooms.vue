79eac63126dce6457d81c79b313319dd580a2744
Lin Pei Wen <mobilesuit0922@gmail.com>
2020-11-16 16:50:33 +0800

    仍然繼續做房間 template，目前先上傳而已

--- src/components/Rooms.vue
+++ src/components/Rooms.vue
@@ -1,21 +1,41 @@
 <template>
     <div>
-        <article class="col-5">
-            <a href="#">查看其他房型</a>
-            <section>
-                <div>
-                    <span>9999</span>
-                    <span>1晚</span>
+        <article
+            class="col-5 bookRoom"
+            :style="{ backgroundImage: `url(${Room1})` }"
+        >
+            <a href="#" class="bookRoom_back bookRoom_grid">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="8.316"
+                    height="15.874"
+                    viewBox="0 0 8.316 15.874"
+                >
+                    <g transform="translate(-14 -3.991)">
+                        <path
+                            class="a"
+                            d="M21.925,3.992a.367.367,0,0,0-.26.114L14.111,11.66a.378.378,0,0,0,0,.534l7.555,7.555a.378.378,0,1,0,.534-.534l-7.288-7.288L22.2,4.64a.377.377,0,0,0,.084-.418A.373.373,0,0,0,21.925,3.992Z"
+                            transform="translate(0 0)"
+                        />
+                    </g>
+                </svg>
+                <span>查看其他房型</span>
+            </a>
+            <section class="bookRoom_booking bookRoom_grid">
+                <div class="bookRoom_booking_price">
+                    <span class="bookRoom_booking_price_money money">9999</span>
+                    <span class="bookRoom_booking_price_night">1晚</span>
                 </div>
-                <button>Booking now</button>
+                <button class="bookingBtn bookingBtn_now">Booking now</button>
             </section>
-            <section>
+            <section class="bookRoom_list bookRoom_grid">
                 <button><i class="fas fa-circle"></i></button>
                 <button><i class="far fa-circle"></i></button>
                 <button><i class="far fa-circle"></i></button>
             </section>
         </article>
-        <article class="col-7">
+        <article class="col-7 roomData">
+            <h1>Single Room</h1>
             <span>1人·單人床·附早餐·衛浴1間·18平方公尺</span>
             <section>
                 <p>平日（一～四）價格：1380 / 假日（五〜日）價格：1500</p>
@@ -24,8 +44,10 @@
             </section>
             <section>
                 <p>・單人間僅供一位客人使用。</p>
-                <p>・臥室配有單人床和私人浴室。 </p>
-                <p>・您需要的一切為您準備：床單和毯子，毛巾，肥皂和洗髮水，吹風機。</p>
+                <p>・臥室配有單人床和私人浴室。</p>
+                <p>
+                    ・您需要的一切為您準備：床單和毯子，毛巾，肥皂和洗髮水，吹風機。
+                </p>
                 <p>・房間裡有AC，當然還有WiFi。</p>
             </section>
             <section>
@@ -50,24 +72,26 @@
     </div>
 </template>
 <script>
+import Room1 from "../assets/image/room1.png";
 export default {
     data() {
         return {
-            roomId: ''
-        }
+            roomId: "",
+            Room1,
+        };
     },
     methods: {
         getRoomData() {
             const vm = this;
             const api = `https://challenge.thef2e.com/api/thef2e2019/stage6/room/${vm.roomId}`;
-            vm.$http.get(api).then(response => {
-                console.log(response.data.room)
-            })
-        }
+            vm.$http.get(api).then((response) => {
+                console.log(response.data.room);
+            });
+        },
     },
     created() {
         this.roomId = this.$route.params.roomId;
         this.getRoomData();
-    }
-}
+    },
+};
 </script>
