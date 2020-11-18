e5f2ddfcc37ae7e5620c1808c305a1bd063881dd
Lin Pei Wen <mobilesuit0922@gmail.com>
2020-11-18 15:11:47 +0800

    房間頁面檢視大致完成，目前在做 order 的頁面

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,16 @@
+<template>
+    <datepicker class="datepicker"></datepicker>
+</template>
+<style lang="scss" scoped>
+    .datepicker {
+        margin-bottom: 1rem;
+    }
+</style>
+<script>
+import datepicker from 'vuejs-datepicker';
+export default {
+    components: {
+        datepicker
+    }
+}
+</script>
